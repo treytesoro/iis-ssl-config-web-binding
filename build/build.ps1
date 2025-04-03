f2496b73a996edb33ad83b008b576c77df07c40a
Trey <104034116+treytesoro@users.noreply.github.com>
2025-04-03 19:47:39 -0400

    Update build.ps1

--- build/build.ps1
+++ build/build.ps1
@@ -2,15 +2,15 @@
 $startFolder = Get-Location
 
 # Locate visual studio's powershell developer shell loader
-$obj = (Get-ChildItem -Path 'C:\Program Files\Microsoft Visual Studio' -Recurse | Where-Object Name -eq 'Launch-VsDevShell.ps1' | Select-Object -First 1)
+# $obj = (Get-ChildItem -Path 'C:\Program Files\Microsoft Visual Studio' -Recurse | Where-Object Name -eq 'Launch-VsDevShell.ps1' | Select-Object -First 1)
 
 # Exit if not found
-if($null -eq $obj){
-    Exit 1
-}
+# if($null -eq $obj){
+#     Exit 1
+# }
 
 # Dot source Launch-VsDevShell.ps1 script
-. "$($obj.Directory.ToString())\$($obj.Name)"
+# . "$($obj.Directory.ToString())\$($obj.Name)"
 
 # Go to solution directory
 Set-Location $startFolder
