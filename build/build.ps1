ed23535f73d21eef9a0c995ed46d8abafe92be04
Trey Tesoro <tgtesoro@sentara.com>
2025-04-04 15:17:45 -0400

    removed passthru

--- build/build.ps1
+++ build/build.ps1
@@ -21,8 +21,8 @@ Set-Location $startFolder
 # Start release build in solution directory
 # Start-Process msbuild -ArgumentList "-p:Configuration=Release"
 Write-Output "Build.ps1 is starting dotnet build -c Release"
-Start-Process dotnet -ArgumentList "restore" -Wait -NoNewWindow -WorkingDirectory $startFolder -PassThru
-Start-Process dotnet -ArgumentList "build", "-c", "Release" -Wait -NoNewWindow -WorkingDirectory $startFolder -PassThru
+Start-Process dotnet -ArgumentList "restore" -Wait -NoNewWindow -WorkingDirectory $startFolder
+Start-Process dotnet -ArgumentList "build", "-c", "Release" -Wait -NoNewWindow -WorkingDirectory $startFolder
 
 
 Set-Location $startFolder
